c8a00d30eb270a1ccfce0900d03f807744bad062
Second_Son_of_Mozzarella <Mohtzee@protonmail.com>
2026-09-14 10:29:26 -0500

    more set up

--- CarPort.Api/obj/Debug/net8.0/CarPort.Api.AssemblyInfo.cs
+++ CarPort.Api/obj/Debug/net8.0/CarPort.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarPort.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5231b3bc7678add741f30629aa53cc43de3bff51")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarPort.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarPort.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
